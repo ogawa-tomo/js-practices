d36814e30e55f36499009e629b590e3532b83068
tomo <o_tomo_1990@yahoo.co.jp>
2023-03-18 20:09:59 +0900

    リファクタ

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -5,12 +5,12 @@ const month = argv.m || now.getMonth() + 1; // 月は0起点
 const year = argv.y || now.getFullYear();
 
 const first_date = new Date(year, month - 1, 1);
-const last_day = new Date(year, month, 0).getDate();
+const last_date = new Date(year, month, 0);
 
 console.log(`     ${month}月 ${year}`);
 console.log("日 月 火 水 木 金 土");
 process.stdout.write("   ".repeat(first_date.getDay()));
-for (let day = 1; day <= last_day; day++) {
+for (let day = 1; day <= last_date.getDate(); day++) {
   let date = new Date(year, month - 1, day);
   process.stdout.write(String(date.getDate()).padStart(2, " ") + " ");
   if (date.getDay() === 6) console.log("");
