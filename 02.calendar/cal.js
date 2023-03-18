fd6ce896fcc420ab8d70128a6e1070a14f9ce47b
tomo <o_tomo_1990@yahoo.co.jp>
2023-03-18 18:03:26 +0900

    月の最初の日から最後の日までプリントする

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,8 +1,14 @@
 var argv = require("minimist")(process.argv.slice(2));
-console.log(argv);
 
-const month = argv.m;
-console.log(month);
+const now = new Date();
+const month = argv.m || now.getMonth() + 1; // 月は0起点
+const year = argv.y || now.getFullYear();
 
-const year = argv.y;
-console.log(year);
+const last_day = new Date(year, month, 0).getDate();
+
+console.log(`     ${month}月 ${year}`);
+console.log("日 月 火 水 木 金 土");
+for (let day = 1; day <= last_day; day++) {
+  let date = new Date(year, month - 1, day);
+  console.log(date.getDate());
+}
