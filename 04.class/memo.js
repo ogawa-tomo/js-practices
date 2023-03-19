35d19405e3e28f3952cf43b5509f4a959cef2bfd
tomo <o_tomo_1990@yahoo.co.jp>
2023-03-20 08:20:45 +0900

    標準入力を受け取る部分を関数化

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -1,15 +1,24 @@
 // process.stdin.resume();
 // process.stdin.setEncoding("utf8");
 
-let lines = [];
-let reader = require("readline").createInterface({
-  input: process.stdin,
-});
-reader.on("line", (line) => {
-  lines.push(line);
-});
-reader.on("close", () => {
-  lines.forEach((line) => {
-    console.log(line);
+const main = async () => {
+  const lines = await getStdinLines();
+  console.log(lines);
+};
+
+const getStdinLines = () => {
+  return new Promise((resolve) => {
+    let lines = [];
+    let reader = require("readline").createInterface({
+      input: process.stdin,
+    });
+    reader.on("line", (line) => {
+      lines.push(line);
+    });
+    reader.on("close", () => {
+      resolve(lines);
+    });
   });
-});
+};
+
+main();
