d3421f89b1485c75c3de6c4f74f7fb049f66e42d
tomo <o_tomo_1990@yahoo.co.jp>
2023-03-20 09:03:57 +0900

    getFilesをメソッドに切り出す

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -7,14 +7,12 @@ const path = require("path");
 const main = async () => {
   const argv = require("minimist")(process.argv.slice(2));
   if (argv.l) {
-    const files = fs.readdirSync(".").filter((fd) => {
-      return (
-        fs.statSync(path.join(".", fd)).isFile() && path.extname(fd) === ".txt"
-      );
-    });
+    const files = getFiles();
     files.forEach((file) => {
       console.log(file.replace(".txt", ""));
     });
+  } else if (argv.r) {
+    const files = getFiles();
   } else {
     const lines = await getStdinLines();
     fs.writeFileSync(`${lines[0]}.txt`, lines.join("\n"));
@@ -34,6 +32,15 @@ const getStdinLines = () => {
       resolve(lines);
     });
   });
+};
+
+const getFiles = () => {
+  const files = fs.readdirSync(".").filter((fd) => {
+    return (
+      fs.statSync(path.join(".", fd)).isFile() && path.extname(fd) === ".txt"
+    );
+  });
+  return files;
 };
 
 main();
