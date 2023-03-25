a14c4bb1b48f0c72a6f9711d299ea41b8ed719d9
tomo <o_tomo_1990@yahoo.co.jp>
2023-03-25 17:55:35 +0900

    変数名をlowerCamelCaseにする

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -16,13 +16,13 @@ const main = () => {
     console.log("年には1970から2100までの数字を指定してください");
     return;
   }
-  const first_date = new Date(year, month - 1, 1);
-  const last_date = new Date(year, month, 0);
+  const firstDate = new Date(year, month - 1, 1);
+  const lastDate = new Date(year, month, 0);
 
   console.log(`     ${month}月 ${year}`);
   console.log("日 月 火 水 木 金 土");
-  process.stdout.write("   ".repeat(first_date.getDay()));
-  for (let day = 1; day <= last_date.getDate(); day++) {
+  process.stdout.write("   ".repeat(firstDate.getDay()));
+  for (let day = 1; day <= lastDate.getDate(); day++) {
     let date = new Date(year, month - 1, day);
     process.stdout.write(String(date.getDate()).padStart(2, " ") + " ");
     if (date.getDay() === 6) console.log("");
